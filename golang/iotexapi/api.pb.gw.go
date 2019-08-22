31d7de961ad0e4003f9ae870abc9746be768f582
lzxm160 <lzxm160@sina.com>
2019-08-22 14:02:49 +0800

    x

--- golang/iotexapi/api.pb.gw.go
+++ golang/iotexapi/api.pb.gw.go
@@ -154,26 +154,6 @@ func request_APIService_ReadContract_0(ctx context.Context, marshaler runtime.Ma
 
 }
 
-var (
-	filter_APIService_ReadContract_1 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
-)
-
-func request_APIService_ReadContract_1(ctx context.Context, marshaler runtime.Marshaler, client APIServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
-	var protoReq ReadContractRequest
-	var metadata runtime.ServerMetadata
-
-	if err := req.ParseForm(); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
-	}
-	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_APIService_ReadContract_1); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
-	}
-
-	msg, err := client.ReadContract(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
-	return msg, metadata, err
-
-}
-
 func request_APIService_SuggestGasPrice_0(ctx context.Context, marshaler runtime.Marshaler, client APIServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq SuggestGasPriceRequest
 	var metadata runtime.ServerMetadata
@@ -554,30 +534,6 @@ func RegisterAPIServiceHandlerClient(ctx context.Context, mux *runtime.ServeMux,
 
 	})
 
-	mux.Handle("GET", pattern_APIService_ReadContract_1, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
-		for k,v:=range pathParams{
-			fmt.Println(k,":",v)
-		}
-		fmt.Println("empty?")
-		ctx, cancel := context.WithCancel(req.Context())
-		defer cancel()
-		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		rctx, err := runtime.AnnotateContext(ctx, mux, req)
-		if err != nil {
-			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
-			return
-		}
-		resp, md, err := request_APIService_ReadContract_1(rctx, inboundMarshaler, client, req, pathParams)
-		ctx = runtime.NewServerMetadataContext(ctx, md)
-		if err != nil {
-			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
-			return
-		}
-
-		forward_APIService_ReadContract_1(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
-
-	})
-
 	mux.Handle("GET", pattern_APIService_SuggestGasPrice_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
@@ -798,8 +754,6 @@ var (
 
 	pattern_APIService_ReadContract_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "readContract"}, "", runtime.AssumeColonVerbOpt(true)))
 
-	pattern_APIService_ReadContract_1 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "readContract"}, "", runtime.AssumeColonVerbOpt(true)))
-
 	pattern_APIService_SuggestGasPrice_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "suggestGasPrice"}, "", runtime.AssumeColonVerbOpt(true)))
 
 	pattern_APIService_EstimateGasForAction_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "estimateGasForAction"}, "", runtime.AssumeColonVerbOpt(true)))
@@ -837,8 +791,6 @@ var (
 	forward_APIService_GetReceiptByAction_0 = runtime.ForwardResponseMessage
 
 	forward_APIService_ReadContract_0 = runtime.ForwardResponseMessage
-
-	forward_APIService_ReadContract_1 = runtime.ForwardResponseMessage
 
 	forward_APIService_SuggestGasPrice_0 = runtime.ForwardResponseMessage
 
